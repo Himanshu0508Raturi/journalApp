0d64bcedfa8b373d9ddbf6ab5fa03c852ce445fc
Himanshu_Raturi <raturihimanshu077@gmail.com>
2025-07-11 20:35:04 +0530

    add comments

--- journalApp/src/main/java/net/engineeringdigest/journalApp/services/WeatherService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/services/WeatherService.java
@@ -17,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherService {
     @Value("${weather.api.key}")
     private String apiKey;
-    @Value("${Weather.api}")
+    @Value("${weather.api.url}")
     private String API;
 
     @Autowired
